2cdb6cafb7b16a2b5de4ec43d371c03b73ea23d0
Bartek <br.wwaww@gmail.com>
2021-03-02 17:02:17 +0100

    added houses, people, parameters

--- main.py
+++ main.py
@@ -27,16 +27,17 @@ class Human:
     dx = 1
     dy = 1
 
-    def __init__(self, x, y, group, action, x_home, y_home, x_work, y_work):
+    def __init__(self, x, y, group, action, homeless, unemployed, x_home, y_home, x_work, y_work):
         self.x = x
         self.y = y
         self.group = group
         self.action = action
+        self.homeless = homeless
+        self.unemployed = unemployed
         self.x_home = x_home
         self.y_home = y_home
         self.x_work = x_work
         self.y_work = y_work
-
 
     def actionGoHome(self):
         if self.x < self.x_home:
@@ -58,8 +59,16 @@ class Human:
         if self.y > self.y_work:
             self.y = self.y - self.dy
 
- #   def actionWalkFree(self):
+#    def actionWalkFree(self):
 
+
+class House:
+    def __init__(self, x, y, dx, dy, no_residents):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.no_residents = no_residents
 
 
 
@@ -98,18 +107,38 @@ Rarray=[]
 
 PPL = []  # [x,y,group,action]
 ACT = np.array(['Home', 'Work', 'WalkFree', 'Movement'])
+HOU=[]
+
+family_size = 3
+homeless_rate = 0.0005
+no_houses = int((N - N * homeless_rate)/family_size)
+unemployment_rate = 0.12
+
 
 
 ### Environment ###
 
-Width = 1000
 Length = 1000
+Width = 1000
 
-for n in range(N):
-    PPL[n, 0] = random.uniform(0, Width)
-    PPL[n, 1] = random.uniform(0, Length)
+# each pixel corresponds to area 5x5 meters.
+
+
+#for n in range(N):
+#    PPL[n, 0] = random.uniform(0, Width)
+#    PPL[n, 1] = random.uniform(0, Length)
 
 ### Parameters ###
+
+contagion_distance = 1
+contagion_probability = 0.9
+incubation_time = 5
+transmission_time = 8
+recovering_time = 20
+ICU_limit = N * 0.05
+
+
+
 
 b = 0.001  # infectious rate, controls the rate of spread which represents the probability of transmitting disease between a susceptible and an infectious individual.
 q = 0.01  # b/g # contact ratio
@@ -135,9 +164,59 @@ Rarray.append(R)
 
 ##################### Main Loop ########################
 
-#for i in N:
-#    PPL.append(Human(1,1,1,1,1,1))
+### Spawn houses ###
+for f in range(no_houses):
+    done1 = False
+    while not done1:
+        x = int(random.uniform(0, Length+1))
+        y = int(random.uniform(0, Width+1))
+        dx = int(random.uniform(1,3))
+        dy = int(random.uniform(1,3))
+        HOU.append(House(x, y, dx, dy, 0))
+        done1 = True
+        
+        # for house in HOU:
+        #     if x - house.x < 10 & x - house.x > -10 & y - house.y < 10 & y - house.y > -10:
+        #         break
+        #     else:
+        #         HOU.append(House(x, y, dx, dy, 0))
+        #         done1 = True
 
+
+### Spawn people ###
+
+for i in range(1):
+    PPL.append(Human(1,1,0,0,1,1,-1,-1,-1,-1))
+
+for i in range(N-2):
+    PPL.append(Human(1,1,0,0,0,0,-1,-1,100,100))
+
+for i in range(1):
+    PPL.append(Human(1,1,2,0,0,0,-1,-1,100,100))
+
+
+for house in HOU:
+    ff = int(random.uniform(2,5))
+    temp = 0
+    for person in PPL:
+        if person.homeless == 0:
+            if person.x_home == -1:
+                person.x_home = house.x
+                person.y_home = house.y
+                person.x = house.x
+                person.y = house.y
+                temp = temp + 1
+                if temp == ff:
+                    break
+
+housesx = []
+housesy = []
+for house in HOU:
+    housesx.append(house.x)
+    housesy.append(house.y)
+    
+plt.plot(housesy, housesx, 'o', color='black')
+plt.show()
 
 
 done = False
@@ -186,6 +265,6 @@ while not done:
     if T == 150:
         done = True
 
-plt.plot(timepassed,Sarray,'g',timepassed,Earray,'y',timepassed,Iarray,'r',timepassed,Rarray,'b')
-plt.show()
+#plt.plot(timepassed,Sarray,'g',timepassed,Earray,'y',timepassed,Iarray,'r',timepassed,Rarray,'b')
+#plt.show()
 
